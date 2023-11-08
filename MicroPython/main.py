de09c8473df96a3e74f99f72a56e5be493f6c9fd
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-11-08 18:00:29 +0000

    annotations

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -15,17 +15,21 @@ display.show(Image.HAPPY)
 sleep(500)
 display.clear()
 
+# loop
 while True:
     if button_a.is_pressed():
         display.clear()
 
+        # set x and y axis to 0
         x_axis = 0
         y_axis = 0
 
+        # loop until x axis is greater than 4
         while x_axis <= 4:
             sleep(500)
             display.clear()
 
+            # make pixels light up diagnally
             display.set_pixel(x_axis, y_axis, 9)
 
             x_axis = x_axis + 1
@@ -37,15 +41,18 @@ while True:
     if button_b.is_pressed():
         display.clear()
 
+        # set x axis to 0 and y axis to 4
         x_axis = 0
         y_axis = 4
 
+        # loop until x axis is greater than 4
         while x_axis <= 4:
             sleep(500)
             display.clear()
 
             display.set_pixel(x_axis, y_axis, 9)
 
+            # make pixels light up diagnally
             x_axis = x_axis + 1
             y_axis = y_axis - 1
 
